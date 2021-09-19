f29d6e9be12c61b0faf943a4c882e2c16c670d9e
Joe Bu <joegbu@gmail.com>
2021-09-19 03:26:58 -0700

    Removed Comments

--- src/AxiosDB/AxiosTest.js
+++ src/AxiosDB/AxiosTest.js
@@ -30,7 +30,6 @@ const AxiosDB = () => {
         }
         const names = []
         for (let i = 0; i < files.length; i++) {
-          // console.log(files[i])
           const name = files[i].key
           if (!(names.includes(name))) {
             names.push(name)
@@ -46,11 +45,8 @@ const AxiosDB = () => {
 
   }, [])
   
-  // console.log(fileNames)
 
   const downloadDB = async () => {
-    // setIsDBLoading(true)
-        // setdbFiles([])
         let dbNames = []
         const listRef = storage.ref()
         const refs = await listRef.listAll()
@@ -58,44 +54,11 @@ const AxiosDB = () => {
             const url = await refs.items[i].getDownloadURL()
             dbNames.push(url)
           }
-          // return dbNames
         return dbNames
   }
 
-  // useEffect(() => {
-  //   // storage.ref().listAll().then( res => {
-  //   //   res.items.forEach( item => item.getDownloadURL().then(
-  //   //     (downloadURLs) => {
-  //   //       setdbFiles(...{ download: downloadURLs, itemList: res.items });
-  //   //       // console.log(dbFiles)
-  //   //     })
-  //   //   )
-  //   // // })
-  //   // setTimeout(() => {
-  //   // if (isApiLoading) {
-  //   //   return
-  //   // }
-  //   // console.log('test2')
-  //   // console.log(fileNames)
-  //   // console.log(fileNames)
-    
-  //   // }, 1000)
-    
-  //     // res.items.forEach((itemRef) =>{
-  //     //   itemRef.getDownloadURL().then((link) => {
-  //         // console.log(link)
-  //         // console.log(typeof link)
-          
-  //   //     })
-  //   //   })
-  //   // })
-  // }, [])
-
-  // console.log("db", dbFiles)
 
   const upload = async (fileNames, dbFiles) => {
-    // console.log('wrrk')
-    // setToAdd([])
     let toUpload = []
     for (let i = 0; i < dbFiles.length; i++) {
       const start = dbFiles[i].indexOf('/o/')
@@ -104,11 +67,8 @@ const AxiosDB = () => {
       if (start !== -1 && end !== -1) {
         filesApi = dbFiles[i].substring(start+3, end)
         console.log(filesApi)
-        // console.log(filesApi)
-        // console.log('test')
         if (!fileNames.includes(filesApi)) {
           toUpload.push(filesApi)
-          // setToAdd(old => [...old, filesApi])
         }
       }
       
@@ -116,48 +76,6 @@ const AxiosDB = () => {
     return toUpload
   }
 
-  // useEffect(() => {
-  //   // setTimeout(() => {
-      
-  //     // setTimeout(() => {
-  //     //   console.log(toAdd.length, dbFiles.length, fileNames.length)
-  //     //   console.log(fileNames)
-  //     // }, 5000)
-      
-
-  //   // }, 1000)
-
-  // }, [isApiLoading, isDBLoading])
- 
-  // const test = listRef.listAll().then((res) => {
-  //   res.items.map((itemRef) =>{
-  //     itemRef.getDownloadURL()
-  //   }
-  // };
-  // console.log(compareFiles(fileNames, dbFiles))
-  // console.log("start test")
-  // // console.log(dbFiles.length)
-  // // console.log(typeof dbFiles.every)
-  // // console.log(dbFiles)
-  // console.log(fileNames)
-  // console.log(files[0])
-  // console.log(dbFiles)
-  // console.log(typeof dbFiles)
-  // console.
-  // let toAdd = []
-  // let filesApi = ''
-
-
-  // console.log("2", dbFiles)
-
-
-  // console.log(test)
-  // for (file in files) {
-  //   for ()
-  //   if file.key
-  // }
-
-  // console.log(test.toString())
   return (
     <div>
       <p>
